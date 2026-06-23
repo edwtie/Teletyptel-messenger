4c46e3d3e037bc956f400e150f23cab664f097fc
Edward Tie <edward.tie@local>
2026-06-23 09:21:30 +0200

    Revert "Show history as full page panel"
    This reverts commit 6b29a62c32245097d67eddf12e1985f435f83efd.

--- php/public/service-worker.js
+++ php/public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "teletyptel-webclient-v341";
-const BUILD_VERSION = "20260623-history-page";
+const CACHE_NAME = "teletyptel-webclient-v340";
+const BUILD_VERSION = "20260623-header-glass";
 const ASSETS = [
   "chat.html",
   "dev.html",
