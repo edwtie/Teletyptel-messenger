b92da699e696ea0639142cc81d3a1d7f7c923a3a
Edward Tie <edward.tie@local>
2026-06-23 10:05:52 +0200

    Match history help button shape

--- php/public/service-worker.js
+++ php/public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "teletyptel-webclient-v356";
-const BUILD_VERSION = "20260623-history-privacy-dialog";
+const CACHE_NAME = "teletyptel-webclient-v357";
+const BUILD_VERSION = "20260623-history-help-square";
 const ASSETS = [
   "chat.html",
   "dev.html",
