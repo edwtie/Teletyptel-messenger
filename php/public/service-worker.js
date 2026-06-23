755372099a81ef024c05ea39e5f8e59388110279
Edward Tie <edward.tie@local>
2026-06-23 09:07:30 +0200

    Keep composer background transparent in light theme

--- php/public/service-worker.js
+++ php/public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "teletyptel-webclient-v335";
-const BUILD_VERSION = "20260623-composer-transparent";
+const CACHE_NAME = "teletyptel-webclient-v336";
+const BUILD_VERSION = "20260623-composer-bg-fix";
 const ASSETS = [
   "chat.html",
   "dev.html",
